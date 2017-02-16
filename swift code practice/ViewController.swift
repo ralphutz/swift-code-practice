a6e63bdac87151b4c4061a3b1d6239c68567a090
Ralph Utz <ralphutz@gmail.com>
2017-02-16 16:58:25 -0600

    string work

--- swift code practice/ViewController.swift
+++ swift code practice/ViewController.swift
@@ -12,3 +12,19 @@ while i < 101 {
     }
     i += 1
 }
+
+
+
+let playground = "Hellow, playground"
+var mutablePlayground = "Hello, mutable playground"
+mutablePlayground += "!"
+
+for c: Character in mutablePlayground.characters {
+    print("'\(c)'")
+}
+
+let oneCoolDude = "\u{1F60E}"
+let aAcute = "\u{0061}\u{0301}"
+for scalar in playground.unicodeScalars {
+    print("\(scalar.value) ")
+}
