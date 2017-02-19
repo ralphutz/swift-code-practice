157adf8453c1f76d9d8c3bf549f4eb34fbb743b4
Ralph Utz <ralphutz@gmail.com>
2017-02-18 20:44:25 -0600

    arrays

--- swift code practice/ViewController.swift
+++ swift code practice/ViewController.swift
@@ -47,3 +47,37 @@ upCaseErrorDescription?.append("Please Try Again")
 upCaseErrorDescription
 
 let description = errorDescription ?? "no error"
+
+
+
+
+var bucketList = ["Climb Mt. Everest"]
+
+var newItems = [
+    "Fly hot air balloon in Fiji",
+    "Watch the Lord of the Rings trilogy in one day",
+    "Go on a walkabout",
+    "Scuba dive in the Great Blue Hole",
+    "Find a triple rainbow"
+]
+
+bucketList += newItems
+
+bucketList.remove(at: 2)
+bucketList
+
+print(bucketList.count)
+print(bucketList[0...2])
+bucketList[2] += " in Australia"
+bucketList
+
+bucketList[0] = "Climb Mt. Kilimanjaro"
+bucketList.insert("Toboggan across Alaska", at: 2)
+bucketList
+
+
+
+
+
+
+
