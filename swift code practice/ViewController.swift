6fc8ad5293cbc3c746ad4983cd041a6028e5c3a5
Ralph Utz <ralphutz@gmail.com>
2017-02-22 16:59:04 -0600

    sets

--- swift code practice/ViewController.swift
+++ swift code practice/ViewController.swift
@@ -131,3 +131,32 @@ print("Alabama has the following zip codes: \(counties)", terminator: "")
 
 
 
+
+
+var groceryBag = Set<String>()
+groceryBag.insert("Apples")
+groceryBag.insert("Oranges")
+groceryBag.insert("Pineapple")
+
+for food in groceryBag {
+    print(food)
+}
+
+let hasBananas = groceryBag.contains("Bananas")
+let friendsGroceryBag = Set(["Bananas", "Cereal", "Milk", "Oranges"])
+let commonGroceryBag = groceryBag.union(friendsGroceryBag)
+let roommatesGroceryBag = Set(["Apples", "Bananas", "Cereal", "Toothpaste"])
+let itemsToReturn = commonGroceryBag.intersection(roommatesGroceryBag)
+
+let yourSecondBag = Set(["Berries", "Yogurt"])
+let roommatesSecondBag = Set(["Grapes", "Honey"])
+let disjoint = yourSecondBag.isDisjoint(with: roommatesSecondBag)
+
+
+let myCities = Set(["Atlanta", "Chicago", "Jacksonville", "New York", "San Francisco"])
+let yourCities = Set(["Chicago", "San Francisco", "Jacksonville"])
+
+let newCities = myCities.isSuperset(of: yourCities)
+
+
+
