380915ef4726682f097e356a24fc942c269169eb
Ralph Utz <ralphutz@gmail.com>
2017-02-25 21:17:11 -0600

    funcs2

--- swift code practice/ViewController.swift
+++ swift code practice/ViewController.swift
@@ -195,5 +195,43 @@ divisionDescriptionFor(numerator: 9.0, denominator: 3.0)
 
 
 
+import Cocoa
+
+func printGreeting () {
+    print("Hello, playground.")
+}
+printGreeting()
+
+func printPersonalGreeting(to names: String...) {
+    for name in names {
+        print("Hello \(name), welcome to your playground.")
+    }
+}
+printPersonalGreeting(to: "Alex", "Chris", "Drew", "Pat" )
+
+func sayHi(to: String) {
+    print("Hi \(to)")
+}
+
+sayHi(to: "Ralph")
+
+func divisionDescriptionFor(numerator: Double,
+                            denominator: Double,
+                            withPunctuation punctuation: String = ".") {
+    print("\(numerator) divided by \(denominator) equals \(numerator / denominator) \(punctuation) ")
+}
+divisionDescriptionFor(numerator: 9.0, denominator: 3.0)
+divisionDescriptionFor(numerator: 9.0, denominator: 3.0, withPunctuation: "!")
+
+var error = "The request failed:"
+func appendErrorCode(_ code: Int, toErrorString errorString: inout String) {
+    if code == 400 {
+        errorString += " bad request."
+    }
+}
+appendErrorCode(400, toErrorString: &error)
+error
+
+
 
 
