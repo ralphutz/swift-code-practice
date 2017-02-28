7b6c94e5f186c23cfa791839c6366283cbc86150
Ralph Utz <ralphutz@gmail.com>
2017-02-27 20:36:22 -0600

    func3

--- swift code practice/ViewController.swift
+++ swift code practice/ViewController.swift
@@ -234,4 +234,17 @@ error
 
 
 
+func areaOfTriangleWith(base: Double, height: Double) -> Double {
+    let numerator = base * height
+    func divide() -> Double {
+        return numerator / 2
+    }
+    return divide()
+}
+areaOfTriangleWith(base: 3.0, height: 5.0)
+
+
+
+
+
 
