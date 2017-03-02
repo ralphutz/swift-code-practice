c67a857fec1c85525096ee75bd31a8a0c9f816aa
Ralph Utz <ralphutz@gmail.com>
2017-03-01 19:07:56 -0600

    guard statements

--- swift code practice/ViewController.swift
+++ swift code practice/ViewController.swift
@@ -254,5 +254,16 @@ if let theName = middleName {
 }
 
 
+func greetByMiddleName(fromFullName name: (first: String,
+    middle: String?,
+    last: String)) {
+    guard let middleName = name.middle else {
+        print("Hey there!")
+        return
+    }
+    print("Hey \(middleName)")
+}
+greetByMiddleName(fromFullName: ("Matt", "Danger", "Mathias"))
+
 
 
